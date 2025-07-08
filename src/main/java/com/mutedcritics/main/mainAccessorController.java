f7191979bd65423865c983966f99cffde5ecc577
KimSeongEun12 <sinamoroll35@gmail.com>
2025-07-08 11:35:26 +0900

    접속자 수 통계 클래스 추가좀 할게여

---
+++ src/main/java/com/mutedcritics/main/mainAccessorController.java
@@ -0,0 +1,15 @@
+package com.mutedcritics.main;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@CrossOrigin
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class mainAccessorController {
+
+}
