6affb502961fc3202c71a1e3f45ddacad9d73da1
xYEONx <kxxn0214@naver.com>
2025-07-09 18:20:36 +0900

    아무거나

--- src/main/java/com/mutedcritics/user/controller/UserGroupController.java
+++ src/main/java/com/mutedcritics/user/controller/UserGroupController.java
@@ -1,16 +1,13 @@
 package com.mutedcritics.user.controller;
 
-import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mutedcritics.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -21,15 +18,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class UserGroupController {
 
-  private final UserService service;
-  
-  @GetMapping(value = "UserGroup/list")
-  public Map<String,Object> UserGroupList(@RequestParam Map<String,Object> param) {
-    
-
-    
-    return null;
-  }
-  
 
 }
