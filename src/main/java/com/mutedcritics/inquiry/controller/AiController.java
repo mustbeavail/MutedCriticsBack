9eb89632e551ac1cd8874eed5cefb9d2fe702341
oneidsin <oneidsin@naver.com>
2025-07-09 13:08:11 +0900

    AI자동답변, 상담사 답변 저장 완료

--- src/main/java/com/mutedcritics/inquiry/controller/AiController.java
+++ src/main/java/com/mutedcritics/inquiry/controller/AiController.java
@@ -8,12 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.mutedcritics.dto.InquiryDTO;
+import com.mutedcritics.dto.ResponseDTO;
 import com.mutedcritics.inquiry.service.AiService;
 
 import lombok.RequiredArgsConstructor;
@@ -45,16 +44,17 @@ public class AiController {
     }
 
     // 상담사가 문의/신고에 대한 답변 작성(아까 받아온 ai 답변을 수정하여 답변 작성 후 저장)
+    // 답변이 등록되었다면 status 를 완료로 변경
     @PostMapping("/inquiry/agent-response")
-    public ResponseEntity<?> saveAgentResponse(@RequestBody InquiryDTO inquiryDTO) {
+    public ResponseEntity<?> saveAgentResponse(@RequestBody ResponseDTO responseDTO) {
         Map<String, Object> result = new HashMap<>();
         try {
-            String agentResponse = service.saveAgentResponse(inquiryDTO);
+            String agentResponse = service.saveAgentResponse(responseDTO);
             result.put("success", true);
             result.put("response", agentResponse);
             return ResponseEntity.ok(result);
         } catch (Exception e) {
-            log.error("상담사 답변 생성 중 오류 발생 inquiryId={}", inquiryDTO.getInquiryIdx(), e);
+            log.error("상담사 답변 생성 중 오류 발생 inquiryId={}", responseDTO.getInquiryIdx(), e);
             result.put("success", false);
             result.put("msg", "상담사 답변 생성 중 오류가 발생했습니다: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
