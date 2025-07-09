47c0ee84bf4b848489b6be7aa1f916531203a1d1
xYEONx <kxxn0214@naver.com>
2025-07-09 16:20:31 +0900

    유저 리스트 불러오기 중

--- src/main/java/com/mutedcritics/user/controller/UserGroupController.java
+++ src/main/java/com/mutedcritics/user/controller/UserGroupController.java
@@ -1,15 +1,35 @@
 package com.mutedcritics.user.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mutedcritics.user.service.UserService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
 
 @CrossOrigin
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 public class UserGroupController {
+
+  private final UserService service;
+  
+  @GetMapping(value = "UserGroup/list")
+  public Map<String,Object> UserGroupList(@RequestParam Map<String,Object> param) {
+    
+
+    
+    return null;
+  }
+  
 
 }
