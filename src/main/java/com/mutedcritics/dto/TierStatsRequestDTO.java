fcaed3cde4d321c31a795b0c8ed80e161c38e708
oneidsin <oneidsin@naver.com>
2025-07-10 16:12:24 +0900

    유저 분류 통계 진행중

--- src/main/java/com/mutedcritics/dto/TierStatsRequestDTO.java
+++ src/main/java/com/mutedcritics/dto/TierStatsRequestDTO.java
@@ -6,9 +6,9 @@ import lombok.Data;
 public class TierStatsRequestDTO {
 
     private int seasonId;
-    private String classification;
-    private String value;
-    private int minPlayTime;
-    private int maxPlayTime;
+    private String gender; // 성별 필터
+    private String region; // 지역 필터
+    private Boolean vip; // VIP 필터
+    private Integer heroId; // 메인 영웅 필터
 
 }
