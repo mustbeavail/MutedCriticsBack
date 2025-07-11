bd121fec80ec10ef83402e352faae7a10198e814
oneidsin <oneidsin@naver.com>
2025-07-11 18:00:48 +0900

    인게임 일일 통계 진행중

--- src/main/java/com/mutedcritics/ingame/service/IngameService.java
+++ src/main/java/com/mutedcritics/ingame/service/IngameService.java
@@ -2,16 +2,27 @@ package com.mutedcritics.ingame.service;
 
 import java.time.LocalDate;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import com.mutedcritics.ingame.dao.IngameDAO;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class IngameService {
 
     private final IngameDAO dao;
+
+    @Transactional
+    public void insertDailyHeroStats(LocalDate startDate, LocalDate endDate) {
+        log.info("일일 영웅 통계 업데이트 시작 : {} ~ {}", startDate, endDate);
+        dao.insertDailyHeroStats(startDate, endDate);
+        log.info("일일 영웅 통계 업데이트 완료 : {} ~ {}", startDate, endDate);
+    }
 
 }
