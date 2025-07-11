9a689a9ec5dafcb28650b25458fe6f22966f4946
mustbeavail <grick21@gmail.com>
2025-07-11 16:35:39 +0900

    포럼 상세보기, 댓글불러오기, 검색 완료

--- src/main/java/com/mutedcritics/forum/repository/ForumCommentRepository.java
+++ src/main/java/com/mutedcritics/forum/repository/ForumCommentRepository.java
@@ -1,9 +1,13 @@
 package com.mutedcritics.forum.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mutedcritics.entity.ForumComment;
 
 public interface ForumCommentRepository extends JpaRepository<ForumComment, Integer> {
+
+    Page<ForumComment> findByForumPostPostIdx(int postIdx, Pageable pageable);
 
 }
--- src/main/java/com/mutedcritics/forum/repository/ForumPostRepository.java
+++ src/main/java/com/mutedcritics/forum/repository/ForumPostRepository.java
@@ -3,11 +3,22 @@ package com.mutedcritics.forum.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mutedcritics.entity.ForumPost;
 
 public interface ForumPostRepository extends JpaRepository<ForumPost, Integer> {
 
     Page<ForumPost> findByTopic(String topic, Pageable pageable);
+
+    @Query("SELECT p FROM ForumPost p WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :search, '%')) AND p.topic = :topic")
+    Page<ForumPost> findByTitleContaining(@Param("search") String search, @Param("topic") String topic, Pageable pageable);
+
+    @Query("SELECT p FROM ForumPost p WHERE LOWER(p.content) LIKE LOWER(CONCAT('%', :search, '%')) AND p.topic = :topic")
+    Page<ForumPost> findByContentContaining(@Param("search") String search, @Param("topic") String topic, Pageable pageable);
+
+    @Query("SELECT p FROM ForumPost p WHERE LOWER(p.user.userId) LIKE LOWER(CONCAT('%', :search, '%')) AND p.topic = :topic")
+    Page<ForumPost> findByUserIdContaining(@Param("search") String search, @Param("topic") String topic, Pageable pageable);
 
 }
