5e2d9e8452eabebb77733d28e0b5f910862c14a6
oneidsin <oneidsin@naver.com>
2025-07-14 14:36:05 +0900

    cors 설정 수정

--- src/main/java/com/mutedcritics/config/GlobalCorsConfig.java
+++ src/main/java/com/mutedcritics/config/GlobalCorsConfig.java
@@ -10,7 +10,7 @@ public class GlobalCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 적용
-                .allowedOrigins("*") // 모든 도메인 허용 (배포 시엔 특정 도메인으로 제한 권장)
+                .allowedOriginPatterns("*") // 모든 도메인 허용 (배포 시엔 특정 도메인으로 제한 권장)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키 허용 시 true
