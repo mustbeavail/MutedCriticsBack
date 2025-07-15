c21918cf73632783f4780a48ff888bc39db082ed
oneidsin <oneidsin@naver.com>
2025-07-15 11:29:59 +0900

    엔티티 수정, 유저 분류 진행중

--- src/main/java/com/mutedcritics/entity/StatsItemDaily.java
+++ src/main/java/com/mutedcritics/entity/StatsItemDaily.java
@@ -1,20 +1,17 @@
 package com.mutedcritics.entity;
 
 import javax.persistence.*;
+
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "stats_item_daily", uniqueConstraints = @UniqueConstraint(columnNames = { "stats_date", "item_idx" }))
+@Table(name = "stats_item_daily", uniqueConstraints = @UniqueConstraint(columnNames = {"stats_date", "item_idx"}))
 @Data
 public class StatsItemDaily {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "item_stats_id")
-    private Long itemStatsId;
-
     @Column(name = "stats_date", nullable = false)
     private LocalDate statsDate;
 
@@ -37,12 +34,4 @@ public class StatsItemDaily {
     @Column(name = "daily_refund_count")
     private int dailyRefundCount;
 
-    @Column(name = "returning_user_buy_count")
-    private int returningUserBuyCount;
-
-    @Column(name = "regular_user_buy_count")
-    private int regularUserBuyCount;
-
-    @Column(name = "vip_user_buy_count")
-    private int vipUserBuyCount;
 }
--- src/main/java/com/mutedcritics/user/controller/UserGroupController.java
+++ src/main/java/com/mutedcritics/user/controller/UserGroupController.java
@@ -1,22 +1,33 @@
 package com.mutedcritics.user.controller;
 
-
-import org.springframework.web.bind.annotation.CrossOrigin;
+import com.mutedcritics.user.service.UserGroupService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDate;
 
-
-
-
-@CrossOrigin
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/user/group")
 public class UserGroupController {
+    private final UserGroupService service;
 
+    @PostMapping("/classify")
+    public ResponseEntity<String> classifyUsers(
+            @RequestParam(value = "baseDate", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate baseDate
+    ) {
+        LocalDate date = baseDate != null ? baseDate : LocalDate.now();
+        log.info("[유저 분류 시작] 기준일 : {}", date);
+        //service.classifyUsers(date);
+        return ResponseEntity.ok("유저 분류 완료 (기준일: ) " + date + ")");
+    }
 
 }
