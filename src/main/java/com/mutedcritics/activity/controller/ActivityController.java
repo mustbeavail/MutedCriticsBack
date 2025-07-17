8f43420bd9d0b4bee2f50dd430e1f01ee83f024f
mustbeavail <grick21@gmail.com>
2025-07-17 17:13:13 +0900

    접속자 통계 수정

--- src/main/java/com/mutedcritics/activity/controller/ActivityController.java
+++ src/main/java/com/mutedcritics/activity/controller/ActivityController.java
@@ -7,10 +7,9 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
