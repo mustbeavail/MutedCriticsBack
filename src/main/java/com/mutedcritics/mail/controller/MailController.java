8e217c8d9a0ebf30b20449410de18ec51d954b29
KimSeongeun12 <elex10147356@gmail.com>
2025-07-20 11:51:57 +0900

    최초 커밋.... (깃허브 가서 다운 받은 거라서 아마 최신일 거예요)

--- src/main/java/com/mutedcritics/mail/controller/MailController.java
+++ src/main/java/com/mutedcritics/mail/controller/MailController.java
@@ -4,9 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +20,6 @@ import com.mutedcritics.mail.service.MailService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
--- src/main/java/com/mutedcritics/mail/repository/UserRepository.java
+++ src/main/java/com/mutedcritics/mail/repository/UserRepository.java
@@ -13,4 +13,8 @@ public interface UserRepository extends JpaRepository<User, String> {
     // userType으로 userId 목록 조회
     @Query("SELECT u.userId FROM User u WHERE u.userType = :userType AND u.receiveYn = true")
     List<String> findUserIdsByUserType(@Param("userType") String userType);
+
+    // 모든 유저 타입 조회
+    @Query("SELECT u.userId FROM User u WHERE u.receiveYn = true AND u.userType != '정지' AND u.userType != '탈퇴'")
+    List<String> findAllUserIds();
 }
