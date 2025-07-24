83a10c8a2598e5448b775d47a2d7b707ae940f2d
oneidsin <oneidsin@naver.com>
2025-07-24 12:50:04 +0900

    외장톰캣으로 구동시 토큰 생성이 되지않는 문제 해결 위해 키 생성 추가

--- src/main/java/com/mutedcritics/ServletInitializer.java
+++ src/main/java/com/mutedcritics/ServletInitializer.java
@@ -3,10 +3,19 @@ package com.mutedcritics;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.mutedcritics.utils.JwtUtil;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("키 생성");
+
+		// pri_key가 없을 경우 생성
+		if (JwtUtil.getPri_key() == null) {
+			JwtUtil.setPri_key();
+		}
+
 		return application.sources(MutedCriticsApplication.class);
 	}
 
